c5b58fdc1b3c68749c48055a2b6f756aa9eb1483
rutu2808 <rutuja.b.nemane2808@gmail.com>
2020-10-15 19:34:29 +0530

    Add file

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify, render_template, request
-import numpy as np;
+import numpy as np
+import pandas as pd
+import sklearn as sk
 import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn.model_selection import train_test_split
+
+
 
 app = Flask(__name__)
 
@@ -11,15 +18,24 @@ def root():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['GET','POST'])
 def predict():
+    df = pd.read_csv('deceptive-opinion.csv')
+    df1 = df[['deceptive', 'text']]
+    df1.loc[df1['deceptive'] == 'deceptive', 'deceptive'] = 0
+    df1.loc[df1['deceptive'] == 'truthful', 'deceptive'] = 1
+    X = df1['text']
+    Y = np.asarray(df1['deceptive'], dtype = int)
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3,random_state=109)
+    cv = CountVectorizer()
+    x = cv.fit_transform(X_train)
+    y = cv.transform(X_test)
+    message = request.form.get('enteredinfo')
+    data = [message]
+    vect = cv.transform(data).toarray()
+    pred = model.predict(vect)
 
-    
-    # pred=model.predict([sf])
-    # return render_template('result.html', prediction_text='The review is "{}"'.format(pred[0]))
-     return render_template('index.html', prediction_text='heelo')
-
-    
+    return render_template('result.html', prediction_text=pred)
 
     
 if __name__ == '__main__':
